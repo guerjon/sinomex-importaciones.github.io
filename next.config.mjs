bcff596a984d9b3e3bbceeb4e0c3870e909d26ad
Jonathan Perez <jonathan_perez4@apple.com>
2024-02-06 13:03:27 -0600

    remove next config options.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/sinomex-importaciones.github.io",
   output: "export",
-  reactStrictMode: true,
 };
 
 export default nextConfig;
